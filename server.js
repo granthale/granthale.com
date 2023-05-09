4a9e632d75284d540261ecbfb9a50fda07d151c2
Grant Hale <granthale@Grants-MacBook-Pro-4.local>
2023-05-09 10:45:04 -0500

    Added favicon

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const favicon = require('favicon')
 const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.static("public")) 
 app.use(express.urlencoded( {extended:true} )) // Allows you to parse body
 app.use(express.json()) // Allows you to parse JSON
+app.use(serveFavicon)
+
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.get('/', function(req, res) {
     res.render(__dirname + '/views/pages/index.ejs');
@@ -26,6 +30,7 @@ const peopleRouter = require('./routes/people')
 app.use('/people', peopleRouter)
 
 const questionsRouter = require('./routes/questions')
+const serveFavicon = require('serve-favicon')
 app.use('/questions', questionsRouter)
 
 const port = process.env.PORT || 4000;
