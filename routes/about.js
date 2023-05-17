e98411c3c2a7cd3353b9cc96a956edcb726a5034
Grant Hale <granthale@Grants-MacBook-Pro-4.local>
2023-05-17 15:53:12 -0500

    Added some new things

--- routes/about.js
+++ routes/about.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render("pages/about")
+    res.render("pages/aboutTest")
 })
 
 module.exports = router
